0591bb14908bf7bcfb4997c95396dbacc910fc8e
Mustafa <mustafcigci@gmail.com>
2020-09-01 15:58:50 +0300

    ikinci satir ekledim

--- src/git_project_ikinci/deneme.java
+++ src/git_project_ikinci/deneme.java
@@ -3,7 +3,9 @@ package git_project_ikinci;
 public class deneme {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("Ikinci git projem 1");
+		System.out.println("Ikinci satir kodum");
 
 	}
 
